0de3cb8cee8fafbd96e9ec3aa5b9cc3103a3b2cf
realdaneesh <daneeshshetty@gmail.com>
2026-02-09 22:46:28 +0530

    Add files via upload

---
+++ q2module.py
@@ -0,0 +1,14 @@
+import math
+
+# Taking input
+num = float(input("Enter a number: "))
+
+# Calculations using math module
+square_root = math.sqrt(num)
+natural_log = math.log(num)
+sine_value = math.sin(num)
+
+# Displaying results
+print("Square root:", square_root)
+print("Natural logarithm (base e):", natural_log)
+print("Sine (in radians):", sine_value)
